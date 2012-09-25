160359f28ab3225ecbd4f21f5db80fba4f3c8605
atmstudent <atmstudent@Administrators-iMac-6.local>
2012-09-25 11:10:02 +0200

    Initial Commit

---
+++ MyRo Cam/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyRo Cam
+//
+//  Created by atmstudent on 9/25/12.
+//  Copyright (c) 2012 wittyworx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MyRo Cam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyRo Cam
+//
+//  Created by atmstudent on 9/25/12.
+//  Copyright (c) 2012 wittyworx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
