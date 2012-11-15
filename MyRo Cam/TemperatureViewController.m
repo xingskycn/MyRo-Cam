53a923bf8f0b8b39a14bbb1ea7770b591693f8bf
atmstudent <atmstudent@Administrators-iMac-3.local>
2012-11-15 10:53:12 +0100

    temperatuur

--- MyRo Cam/TemperatureViewController.m
+++ MyRo Cam/TemperatureViewController.m
@@ -10,12 +10,12 @@
 #import "Temperature.h"
 
 @interface TemperatureViewController ()
-
 @end
 
 @implementation TemperatureViewController
 
 @synthesize lblTemperature;
+@synthesize tableData;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,6 +29,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self parseJSON];
 	// Do any additional setup after loading the view.
     Temperature *temp = [[Temperature alloc] init];
     
@@ -44,5 +46,33 @@
 - (void)viewDidUnload {
     [self setLblTemperature:nil];
     [super viewDidUnload];
+}
+
+-(void)parseJSON {
+    NSData *dataUrl = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.wunderground.com/api/29c3df9966dfd8c1/conditions/q/CA/Goes.json"]];
+    NSInputStream *dataInputStream = [[NSInputStream alloc] initWithData:dataUrl];
+    [dataInputStream open];
+    if (dataInputStream) {
+        
+        NSError *parseError = nil;
+        
+        id jsonObject = [NSJSONSerialization JSONObjectWithStream:dataInputStream options:NSJSONReadingAllowFragments error:&parseError];
+        if ([jsonObject respondsToSelector:@selector(objectForKey:)]) {
+            
+        
+            NSDictionary *section = [jsonObject objectForKey:@"current_observation"];
+            //NSString temp = [NSString stringWithFormat:@" Temperatuur: %s",];
+            
+           // NSArray *result = [section objectForKey:@"temp_c"];
+            
+         NSDictionary *section2 = [section objectForKey:@"temp_c"];
+            
+             
+            
+            
+        }
+        
+
+    }
 }
 @end
